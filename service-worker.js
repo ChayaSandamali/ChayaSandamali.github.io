719c283b9384d0de8415fd7ea79c74a113dc2f2e
Chaya Sandamali <chayasan14@gmail.com>
2015-11-30 07:36:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -30,7 +30,7 @@
 // cache, then increment the CACHE_VERSION value. It will kick off the service worker update
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
-var CACHE_VERSION = 16;
+var CACHE_VERSION = 17;
 var CURRENT_CACHES = {
     prefetch: 'prefetch-cache-v' + CACHE_VERSION
 };
@@ -39,7 +39,7 @@ self.addEventListener('install', function(event) {
     var now = Date.now();
 
     var urlsToPrefetch = [
-        'home.html',
+        'serviceWorker.html',
         'sample1.html'
         // This is an image that will be used in pre_fetched.html
         //'project.html'
